5d8a1653cb838ccbb0e02c11deab994b72c8c107
Keith Davidson <kbdsp@hotmail.com>
2021-11-20 23:29:03 -0500

    Start of 169, finished productitem

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
+import Colors from '../../constants/Colors';
+
 const ProductItem = (props) => {
     return (
         <View style={styles.product}>
-            <Image style={styles.image} source={{ uri: props.image }} />
-            <Text style={styles.title}>{props.title}</Text>
-            <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={{ uri: props.image }} />
+            </View>
+            <View style={styles.details}>
+                <Text style={styles.title}>{props.title}</Text>
+                <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            </View>
             <View style={styles.actions}>
-                <Button title="View Details" onPress={props.onViewDetail} />
-                <Button title="To Cart" onPress={props.onAddToCart} />
+                <Button color={Colors.primary} title="View Details" onPress={props.onViewDetail} />
+                <Button color={Colors.primary} title="To Cart" onPress={props.onAddToCart} />
             </View>
         </View>
     );
@@ -29,10 +35,23 @@ const styles = StyleSheet.create({
         height: 300,
         margin: 20,
     },
-    image: {
+    imageContainer: {
         width: '100%',
         height: '60%',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        overflow: 'hidden',
     },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+    details: {
+        alignItems: 'center',
+        height: '15%',
+        padding: 10,
+    },
+
     title: {
         fontSize: 18,
         marginVertical: 4,
@@ -45,7 +64,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
-
+        height: '25%',
+        paddingHorizontal: 20,
     }
 
 });
