7b96e52af85cd09ae12e57d67ec15ac93644638b
Keith Davidson <kbdsp@hotmail.com>
2021-11-21 22:51:32 -0500

    Start of 173, added add_to_cart handler

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import * as Font from 'expo-font';
+// import { composeWithDevTools } from 'redux-devtools-extension'; // For react devtools
 import AppLoading from 'expo-app-loading';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
 import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer,
+  cart: cartReducer,
 
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer); // , composeWithDevTools());
 
 const fetchFonts = () => {
   return Font.loadAsync({
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -3,21 +3,24 @@ import { ScrollView, Image, Button, StyleSheet, Text, View } from 'react-native'
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import * as cartActions from '../../store/actions/cart';
 
 import Colors from '../../constants/Colors';
+import cart from '../../store/reducers/cart';
 
 const ProductDetailScreen = (props) => {
     const productId = props.navigation.getParam('productId');
     const selectedProduct = useSelector(state =>
         state.products.availableProducts.find(prod => prod.id === productId)
     );
+    const dispatch = useDispatch();
 
     return (
         <ScrollView>
             <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
             <View style={styles.actions}>
-                <Button color={Colors.primary} title="Add to Cart" onPress={() => { }} />
+                <Button color={Colors.primary} title="Add to Cart" onPress={() => { dispatch(cartActions.addToCart(selectedProduct))}} />
             </View>
             <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
             <Text style={styles.description}>{selectedProduct.description}</Text>
