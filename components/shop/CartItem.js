97807635503e35ddd8fbab104c309bf2b50f0d1a
Keith Davidson <kbdsp@hotmail.com>
2021-11-26 22:23:02 -0500

    Start of 175, added cart item listing

---
+++ components/shop/CartItem.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const CartItem = (props) => {
+    return (
+        <View style={styles.cartItem}>
+            <Text style={styles.itemData}>
+                <Text style={styles.quantity}>{props.quantity} </Text>
+                <Text style={styles.mainText}>{props.title}</Text>
+            </Text>
+            <View style={styles.itemData}>
+                <Text style={styles.mainText}>${props.amount.toFixed(2)}</Text>
+                <TouchableOpacity style={styles.deleteButton} onPress={props.onRemove}>
+                    <Ionicons
+                        name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'}
+                        size={23}
+                        color="red"
+                    />
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    );
+};
+
+export default CartItem;
+
+const styles = StyleSheet.create({
+    cartItem: {
+        padding: 10,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginHorizontal: 20,
+    },
+    itemData: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    quantity: {
+        fontFamily: 'open-sans',
+        color: '#888',
+        fontSize: 16,
+    },
+    mainText: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 16
+    },
+    deleteButton: {
+        marginLeft: 20
+    },
+
+});
--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import { useSelector } from 'react-redux';
 import Colors from '../../constants/Colors';
+import CartItem from '../../components/shop/CartItem';
 
 const CartScreen = (props) => {
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -11,7 +12,8 @@ const CartScreen = (props) => {
             transformedCartItems.push({
                 productId: key,
                 productTitle: state.cart.items[key].productTitle,
-                productPrice: state.cart.items[key].quantity,
+                productPrice: state.cart.items[key].productPrice,
+                quantity: state.cart.items[key].quantity,
                 sum: state.cart.items[key].sum,
             });
         }
@@ -22,11 +24,21 @@ const CartScreen = (props) => {
         <View style={styles.screen}>
             <View style={styles.summary}>
                 <Text style={styles.summaryText}>Total: <Text style={styles.amount}>${cartTotalAmount.toFixed(2)}</Text></Text>
-                <Button color={Colors.accent} title="Order Now" disabled={cartItems.length === 0}/>
+                <Button color={Colors.accent} title="Order Now" disabled={cartItems.length === 0} />
             </View>
-            <View>
-                <Text>Cart Items</Text>
-            </View>
+            <FlatList
+                data={cartItems}
+                keyExtractor={item => item.productId}
+                renderItem={itemData => (
+                    <CartItem
+                        quantity={itemData.item.quantity}
+                        title={itemData.item.productTitle}
+                        amount={itemData.item.sum}
+                        onRemove={() => {}}
+                    />
+                )}
+            />
+
         </View>
     );
 };
