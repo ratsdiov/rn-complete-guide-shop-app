829b76b9a4ad2b435de8172b8147716d9155f506
Keith Davidson <kbdsp@hotmail.com>
2021-12-22 20:19:07 -0500

    Start of 205, added storing new items on server

--- store/actions/products.js
+++ store/actions/products.js
@@ -8,14 +8,34 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-    return {
-        type: CREATE_PRODUCT,
-        productData: { // Note the syntax below - same as title: title, etc
-            title,
-            description,
-            imageUrl,
-            price
-        }
+    return async dispatch => {
+        const response = await fetch("https://rn-shop-app-a7346-default-rtdb.firebaseio.com/products.json", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                imageUrl,
+                price
+            })
+        });
+
+        const resData = await response.json();
+
+        // console.log('resdata', resData);
+
+        dispatch({
+            type: CREATE_PRODUCT,
+            productData: { // Note the syntax below - same as title: title, etc
+                id: resData.name,
+                title,
+                description,
+                imageUrl,
+                price
+            }
+        });
     };
 };
 
