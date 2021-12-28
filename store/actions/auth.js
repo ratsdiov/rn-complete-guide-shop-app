224f1717ee3e3165059c2f63580aed7fceb58f51
Keith Davidson <kbdsp@hotmail.com>
2021-12-27 22:11:55 -0500

    Start of 227, added auto-login

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,7 +1,12 @@
-export const SIGNUP = 'SIGNUP';
-export const LOGIN = 'LOGIN';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Todo - combine the highly duplicated logic between signup and
+export const AUTHENTICATE = 'AUTHENTICATE';
+
+export const authenticate = (userId, token) => {
+    return { type: AUTHENTICATE, userId: userId, token: token };
+};
+
+// Todo - combine the highly duplicated logic between signup and login
 
 export const signup = (email, password) => {
     return async dispatch => {
@@ -20,7 +25,6 @@ export const signup = (email, password) => {
             }
         );
 
-
         if (!response.ok) {
             const errorResData = await response.json();
             const errorId = errorResData.error.message;
@@ -35,6 +39,9 @@ export const signup = (email, password) => {
         // console.log('signup', resData);
 
         dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
+
+        const expirationDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000);
+        saveDataToStorage(resData.idToken, resData.localId, expirationDate);
 
     };
 };
@@ -57,7 +64,6 @@ export const login = (email, password) => {
             }
         );
 
-
         if (!response.ok) {
             const errorResData = await response.json();
             const errorId = errorResData.error.message;
@@ -73,7 +79,19 @@ export const login = (email, password) => {
         const resData = await response.json();
         // console.log('login', resData);
 
-        dispatch({ type: LOGIN, token: resData.idToken, userId: resData.localId });
-
+        dispatch(authenticate(resData.localId, resData.idToken));
+        const expirationDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000);
+        saveDataToStorage(resData.idToken, resData.localId, expirationDate);
     };
+};
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+    AsyncStorage.setItem(
+        'userData',
+        JSON.stringify({
+            token: token,
+            userId: userId,
+            expiryDate: expirationDate.toISOString(),
+        })
+    );
 };
